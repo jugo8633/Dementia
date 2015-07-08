c04eec01fa1157a2e2390a9703643d435a3cecb5
jugo8633 <jugo.tw@gmail.com>
2015-07-08 21:16:45 +0800

    add

---
+++ src/medical/dementia/MainActivity.java
@@ -0,0 +1,16 @@
+package medical.dementia;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
